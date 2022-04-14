c30e08a54e0e26b3b076e4241c78c5ea54132bb2
Kaustav Jaiswal <kaustav.jaiswal@cred.club>
2022-04-15 02:09:26 +0530

    clean up utils to remove gltfio dependency

--- android/filament-utils-android/src/main/cpp/Utils.cpp
+++ android/filament-utils-android/src/main/cpp/Utils.cpp
@@ -31,65 +31,6 @@ using namespace image;
 jlong nCreateHDRTexture(JNIEnv* env, jclass,
         jlong nativeEngine, jobject javaBuffer, jint remaining, jint internalFormat);
 
-static jlong nCreateKTXTexture(JNIEnv* env, jclass,
-        jlong nativeEngine, jobject javaBuffer, jint remaining, jboolean srgb) {
-    Engine* engine = (Engine*) nativeEngine;
-    AutoBuffer buffer(env, javaBuffer, remaining);
-    KtxBundle* bundle = new KtxBundle((const uint8_t*) buffer.getData(), buffer.getSize());
-    return (jlong) ktx::createTexture(engine, *bundle, srgb, [](void* userdata) {
-        KtxBundle* bundle = (KtxBundle*) userdata;
-        delete bundle;
-    }, bundle);
-}
-
-static jlong nCreateIndirectLight(JNIEnv* env, jclass,
-        jlong nativeEngine, jobject javaBuffer, jint remaining, jboolean srgb) {
-    Engine* engine = (Engine*) nativeEngine;
-    AutoBuffer buffer(env, javaBuffer, remaining);
-    KtxBundle* bundle = new KtxBundle((const uint8_t*) buffer.getData(), buffer.getSize());
-    Texture* cubemap = ktx::createTexture(engine, *bundle, srgb,  [](void* userdata) {
-        KtxBundle* bundle = (KtxBundle*) userdata;
-        delete bundle;
-    }, bundle);
-
-    float3 harmonics[9];
-    bundle->getSphericalHarmonics(harmonics);
-
-    IndirectLight* indirectLight = IndirectLight::Builder()
-        .reflections(cubemap)
-        .irradiance(3, harmonics)
-        .intensity(30000)
-        .build(*engine);
-
-    return (jlong) indirectLight;
-}
-
-static jlong nCreateSkybox(JNIEnv* env, jclass,
-        jlong nativeEngine, jobject javaBuffer, jint remaining, jboolean srgb) {
-    Engine* engine = (Engine*) nativeEngine;
-    AutoBuffer buffer(env, javaBuffer, remaining);
-    KtxBundle* bundle = new KtxBundle((const uint8_t*) buffer.getData(), buffer.getSize());
-    Texture* cubemap = ktx::createTexture(engine, *bundle, srgb,  [](void* userdata) {
-        KtxBundle* bundle = (KtxBundle*) userdata;
-        delete bundle;
-    }, bundle);
-    return (jlong) Skybox::Builder().environment(cubemap).showSun(true).build(*engine);
-}
-
-static jboolean nGetSphericalHarmonics(JNIEnv* env, jclass, jobject javaBuffer, jint remaining,
-        jfloatArray outSphericalHarmonics_) {
-    AutoBuffer buffer(env, javaBuffer, remaining);
-    KtxBundle bundle((const uint8_t*) buffer.getData(), buffer.getSize());
-
-    jfloat* outSphericalHarmonics = env->GetFloatArrayElements(outSphericalHarmonics_, nullptr);
-    const auto success = bundle.getSphericalHarmonics(
-        reinterpret_cast<filament::math::float3*>(outSphericalHarmonics)
-    );
-    env->ReleaseFloatArrayElements(outSphericalHarmonics_, outSphericalHarmonics, JNI_ABORT);
-
-    return success ? JNI_TRUE : JNI_FALSE;
-}
-
 JNIEXPORT jint JNI_OnLoad(JavaVM* vm, void*) {
     JNIEnv* env;
     if (vm->GetEnv(reinterpret_cast<void**>(&env), JNI_VERSION_1_6) != JNI_OK) {
@@ -97,18 +38,6 @@ JNIEXPORT jint JNI_OnLoad(JavaVM* vm, void*) {
     }
 
     int rc;
-
-    // KTXLoader
-    jclass ktxloaderClass = env->FindClass("com/google/android/filament/utils/KTXLoader");
-    if (ktxloaderClass == nullptr) return JNI_ERR;
-    static const JNINativeMethod ktxMethods[] = {
-        {(char*)"nCreateKTXTexture", (char*)"(JLjava/nio/Buffer;IZ)J", reinterpret_cast<void*>(nCreateKTXTexture)},
-        {(char*)"nCreateIndirectLight", (char*)"(JLjava/nio/Buffer;IZ)J", reinterpret_cast<void*>(nCreateIndirectLight)},
-        {(char*)"nCreateSkybox", (char*)"(JLjava/nio/Buffer;IZ)J", reinterpret_cast<void*>(nCreateSkybox)},
-        {(char*)"nGetSphericalHarmonics", (char*)"(Ljava/nio/Buffer;I[F)Z", reinterpret_cast<void*>(nGetSphericalHarmonics)},
-    };
-    rc = env->RegisterNatives(ktxloaderClass, ktxMethods, sizeof(ktxMethods) / sizeof(JNINativeMethod));
-    if (rc != JNI_OK) return rc;
 
     // HDRLoader
     jclass hdrloaderClass = env->FindClass("com/google/android/filament/utils/HDRLoader");
